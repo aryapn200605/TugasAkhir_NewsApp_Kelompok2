9a541af24d57cf5bece7cea7018ca2b58a191092
Deus <syahidazharazizi@gmail.com>
2023-02-28 15:38:58 +0700

    syahid the great

---
+++ app/src/main/java/com/example/tugasakhir_newsapp_kelompok2/ui/newsdetails/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tugasakhir_newsapp_kelompok2.ui.newsdetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tugasakhir_newsapp_kelompok2.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
